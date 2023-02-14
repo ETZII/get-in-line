7b37436b48db420cc036dd8ad63a034d6b9cd63a
YE JI KIM <yejinera@gmail.com>
2023-02-15 00:02:18 +0900

    Error 핸들링 위한 기본 설정
    ErrorCode Enum 작성

--- src/main/java/com/etzii/getinline/controller/BaseController.java
+++ src/main/java/com/etzii/getinline/controller/BaseController.java
@@ -2,12 +2,18 @@ package com.etzii.getinline.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class BaseController implements ErrorController {
-    @RequestMapping("/error")
-    public String error() {
-        return "error";
+public class BaseController {
+
+    @GetMapping("/")
+    public String root() throws Exception {
+        throw new Exception("테스트");
     }
+//    @RequestMapping("/error")
+//    public String error() {
+//        return "error";
+//    }
 }
